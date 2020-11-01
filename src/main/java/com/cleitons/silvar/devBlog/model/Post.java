0c882651f668225576eab21526ab781f987df170
Cleiton Silva <cleiton_.98@hotmail.com>
2020-11-01 14:44:24 -0300

    commit inicial

---
+++ src/main/java/com/cleitons/silvar/devBlog/model/Post.java
@@ -0,0 +1,31 @@
+package com.cleitons.silvar.devBlog.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter private Long id;
+
+    @Column(name = "titulo", nullable = false)
+    @Getter @Setter private String titulo;
+
+    @Column(name = "autor", nullable = false)
+    @Getter @Setter private String autor;
+
+    @Lob
+    @Column(name = "texto", nullable = false)
+    @Getter @Setter private String texto;
+
+    @Column(name = "data_cadastro")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @Getter @Setter private LocalDate dataCadastro;
+}
